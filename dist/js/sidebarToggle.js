bd258a7491f52e3a2d1c6be5096adbe63b34df83
ehsan <ehsanmavaei@gmail.com>
2021-01-13 10:14:19 +0330

    make the sidebar disappear on small screens
    plus some refactoring

--- dist/js/sidebarToggle.js
+++ dist/js/sidebarToggle.js
@@ -2,6 +2,27 @@ const burgerMenu = document.getElementById("burger");
 const container = document.getElementById("container");
 const aside = document.getElementById("aside");
 
+/*thsi function will change the class of sidebar and 
+container so that sidebar won't be shown on screen sizes samaller than 760px*/
+function size(){
+	var value=window.innerWidth;
+    if( value<760){
+	console.log('less than 760');
+	container.classList.add("active");
+	aside.classList.add("active-aside");
+	burgerMenu.classList.add("burger");
+	 
+
+}
+else {
+	aside.classList.remove("active-aside");
+	container.classList.remove("active");
+	burgerMenu.classList.remove("burger");
+
+}
+}
+
+
 // Sidebar Menu Toggle
 burgerMenu.addEventListener("click", () => {
 	burgerMenu.classList.toggle("change");
@@ -14,4 +35,4 @@ burgerMenu.addEventListener("click", () => {
 		aside.classList.add("active-aside");
 		burgerMenu.classList.add("burger");
 	}
-});
+})
